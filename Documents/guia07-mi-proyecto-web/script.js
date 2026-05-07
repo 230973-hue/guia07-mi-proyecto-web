9debd3ac161ff9596c1e1403fa474b1b8819c49a
baruc_ep <230973@unsaac.edu.pe>
2026-05-07 07:47:10 -0500

    Mejora en la interfaz

--- Documents/guia07-mi-proyecto-web/script.js
+++ Documents/guia07-mi-proyecto-web/script.js
@@ -1,10 +1,9 @@
 function saludar() {
-// Obtener el valor del input
-let nombre = document.getElementById("nombre").value;
-// Validar si el usuario escribió algo
+let nombre = document.getElementById("nombre").value.trim();
+
 if (nombre === "") {
 document.getElementById("resultado").innerText =
-"Por favor, ingresa tu nombre.";
+"Por favor, ingresa un nombre válido.";
 } else {
 document.getElementById("resultado").innerText =
 "Hola " + nombre + ", bienvenido al sistema.";
@@ -12,10 +11,16 @@ document.getElementById("resultado").innerText =
 }
 
 function validarCorreo() {
-let correo = document.getElementById("correo").value;
+let correo = document.getElementById("correo").value.trim();
+
 if (correo === "") {
-document.getElementById("mensajeCorreo").innerText = "Debe ingresar un correo.";
+document.getElementById("mensajeCorreo").innerText =
+"Debe ingresar un correo.";
+} else if (!correo.includes("@") || !correo.includes(".")) {
+document.getElementById("mensajeCorreo").innerText =
+"Correo no válido.";
 } else {
-document.getElementById("mensajeCorreo").innerText = "Correo registrado correctamente.";
+document.getElementById("mensajeCorreo").innerText =
+"Correo registrado correctamente.";
 }
 }
